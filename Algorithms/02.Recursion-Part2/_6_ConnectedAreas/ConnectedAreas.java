d60f39a9aa1a12a4e1596468fb3cd7c6785c3123
Gyokan <gyokan.nezhdetov@gmail.com>
2018-02-18 14:20:45 +0200

    The solution now is correct

--- Algorithms/02.Recursion-Part2/_6_ConnectedAreas/ConnectedAreas.java
+++ Algorithms/02.Recursion-Part2/_6_ConnectedAreas/ConnectedAreas.java
@@ -1,11 +1,13 @@
 package _6_ConnectedAreas;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ConnectedAreas {
 
-    private static final char EMPTY_CELL = ' ';
-    private static final char VISITED_CELL = 'V';
+    private static final char EMPTY_CELL = '-';
+    private static final char VISITED_CELL = '*';
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,12 +21,67 @@ public class ConnectedAreas {
             }
         }
 
-        // TODO: implement the logic!
+        ArrayList<Area> areas = new ArrayList<>();
+        int[] coordinates = new int[2];
+        while (findFirstUnvisited(matrix, coordinates)) {
+            int startRow = coordinates[0];
+            int startCol = coordinates[1];
+            Area area = new Area(startRow, startCol, 0);
+            traverseArea(matrix, area, startRow, startCol);
+            areas.add(area);
+        }
+
+        Collections.sort(areas);
+        System.out.println("Total areas found: " + areas.size());
+        int areaNumber = 1;
+        for (Area area : areas) {
+            System.out.printf("Area #%d at (%d, %d), size: %d%n",
+                    areaNumber, area.getRow(), area.getCol(), area.getSize());
+            areaNumber++;
+
+        }
+
+    }
+
+    private static void traverseArea(char[][] matrix, Area area, int row, int col) {
+        if (!isInRange(matrix, row, col)) {
+            return;
+        }
+
+        if (matrix[row][col] != EMPTY_CELL) {
+            return;
+        }
+
+        matrix[row][col]= VISITED_CELL;
+        area.incrementSize();
+        traverseArea(matrix, area, row - 1, col);
+        traverseArea(matrix, area, row, col - 1);
+        traverseArea(matrix, area, row + 1, col);
+        traverseArea(matrix, area, row, col + 1);
+    }
+
+    private static boolean isInRange(char[][] matrix, int row, int col) {
+        boolean isRowInRange = 0 <= row && row < matrix.length;
+        boolean isColumnInRange = 0 <= col && col < matrix[0].length;
+        return isRowInRange && isColumnInRange;
+    }
+
+    private static boolean findFirstUnvisited(char[][] matrix, int[] coordinates) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                if (matrix[row][col] == EMPTY_CELL) {
+                    coordinates[0] = row;
+                    coordinates[1] = col;
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
 
 class Area implements Comparable<Area> {
-
     private int row;
     private int col;
     private int size;
@@ -34,21 +91,24 @@ class Area implements Comparable<Area> {
         this.col = startCol;
         this.size = size;
     }
-
-    private int getRow() {
+    public int getRow() {
         return row;
     }
 
-    private int getCol() {
+    public int getCol() {
         return col;
     }
 
-    private int getSize() {
+    public int getSize() {
         return size;
+    }
+
+    public void incrementSize() {
+        this.size += 1;
     }
 
     @Override
     public int compareTo(Area other) {
-        return Integer.compare(this.getSize(), other.getSize());
+        return Integer.compare(other.getSize(), this.getSize());
     }
 }
